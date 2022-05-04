0b7e7faad46be58bb8c13597a0023f49713ef6b9
hokuson <git config --global user.email>
2022-05-04 22:41:51 +0900

    comit

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-$("#openbtn").on("click", function (event) {
-  //ボタンがクリックされたら
-  event.stopPropagation();
-  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
-  $("#g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
-  $("#g-nav-leader").toggleClass("panelactive");
+$(".openbtn2").click(function () {
+  $(this).toggleClass("active");
+  $("#g-nav").toggleClass("panelactive");
+  $("#g-nav-menu").toggleClass("panelactive");
 });
-
-// $("#header-title").on("click", function () {
-//   console.log("click");
-// });
 
 $("#g-nav a").click(function () {
   //ナビゲーションのリンクがクリックされたら
   $(".openbtn1").removeClass("active"); //ボタンの activeクラスを除去し
   $("#g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+  $("#g-nav-menu").removeClass("panelactive");
 });
